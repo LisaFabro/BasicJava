ec663569188ba83300c57c1c7567670737ea0221
LisaFabro <fabro.lisa@gmail.com>
2023-11-05 15:07:13 +0100

    add cap2 es2 (modifiche per caricarlo come es 3)

--- Cap2_es2/src/Main.java
+++ Cap2_es2/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Objects;
 
 public class Main {
 public static void main(String[] args){
-    Car car1 = new Car(2000, "AA456WW", "Mercedes", "classe A");
+    Car car1 = new Car(2200, "AA456WW", "Mercedes", "classe A");
     System.out.println(car1);
 
     car1.setPlate("BB000CC");
